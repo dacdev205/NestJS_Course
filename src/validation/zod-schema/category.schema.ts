1d83acf20c29c896b757c59c61765187c07e5bc8
dacdev205 <datdev252@gmail.com>
2024-12-30 08:32:34 +0700

    remove empty files

--- src/validation/zod-schema/category.schema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-export const CreateCategorySchema = z.object({
-  name: z.string().min(3, { message: 'Name is required!' }),
-  discription: z.string().min(3, { message: 'Discription is required!' }),
-});
-export type CreateCategorySchema = z.infer<typeof CreateCategorySchema>;
