5888f9ec96ebeab6ea8ea796096c2195b6ae2302
dacdev205 <datdev252@gmail.com>
2025-01-02 11:13:53 +0700

    remove empty files

--- prisma/migrations/20241225021031_/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "brand" ADD COLUMN     "isDeleted" BOOLEAN NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE "category" ADD COLUMN     "isDeleted" BOOLEAN NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE "product" ADD COLUMN     "isDeleted" BOOLEAN NOT NULL DEFAULT false;
