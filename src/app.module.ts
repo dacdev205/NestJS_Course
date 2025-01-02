53d528c34cf474d6f614d13914a5057c4f9a0e48
dacdev205 <datdev252@gmail.com>
2025-01-02 11:25:39 +0700

    add new module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module, ValidationPipe } from '@nestjs/common';
+
+import { APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { CategoryModule } from './module/category/category.module';
+import { AuthModule } from './auth/auth.module';
 import { BrandModule } from './module/brand/brand.module';
+import { CategoryModule } from './module/category/category.module';
+import { ConfigModule } from '@nestjs/config';
 import { ProductModule } from './module/product/product.module';
-import { APP_PIPE } from '@nestjs/core';
+import { UsersModule } from './module/users/users.module';
 
 @Module({
   imports: [
@@ -15,6 +18,8 @@ import { APP_PIPE } from '@nestjs/core';
     CategoryModule,
     BrandModule,
     ProductModule,
+    UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [
