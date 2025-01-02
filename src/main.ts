e1e5186e8ac515755c98d5ab5d5d1a9d47d8c28d
dacdev205 <datdev252@gmail.com>
2025-01-02 11:24:58 +0700

    add global prefix for every HTTP route path

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1', { exclude: [''] });
   await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
