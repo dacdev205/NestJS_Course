94dc65e53646d8dd9d64f38ada62fa5b49ed3661
dacdev205 <datdev252@gmail.com>
2025-01-02 11:33:18 +0700

    config errors

---
+++ src/content/errors/user.error.ts
@@ -0,0 +1,14 @@
+export const USER_NOT_FOUND = {
+  code: 'user_not_found',
+  message: 'User not found',
+};
+
+export const EMAIL_ALREADY_EXIST = {
+  code: 'email_already_exist',
+  message: 'Email already exist',
+};
+
+export const PHONE_ALREADY_EXIST = {
+  code: 'phone_already_exist',
+  message: 'Phone already exist',
+};
