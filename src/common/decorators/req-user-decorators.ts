9425b3d3eb66e3f761dd2d85d2433820ecd7a71e
dac <93912537+dacdev205@users.noreply.github.com>
2025-01-21 20:40:07 +0700

    feat: create request user decorators

--- src/common/decorators/req-user-decorators.ts
+++ src/common/decorators/req-user-decorators.ts
@@ -1,8 +1,8 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 
 export const CurrentUser = createParamDecorator(
-  (data: unknown, context: ExecutionContext) => {
-    const ctx = context.switchToHttp().getRequest();
-    return ctx.user;
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user;
   },
 );
